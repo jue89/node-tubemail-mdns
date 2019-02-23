f650f843932a8acb6bd7d7744859595dfc7a24c1
Jue <me@jue.yt>
2019-02-23 21:54:56 +0100

    Adopted to new discovery server API

--- mdns.js
+++ mdns.js
@@ -1,12 +1,16 @@
+const util = require('util');
+const debug = util.debuglog('tubemail-mdns');
 const Bonjour = require('bonjour-hap');
 
-module.exports = (opts = {}) => (port, needle, cb) => {
+module.exports = (hood, cb) => {
 	const mdns = new Bonjour();
 
-	const type = `tm-${needle.slice(0, 12)}`;
-	const name = opts.name || `Tubemail Peer ${port}`
+	const port = hood.port;
+	const type = `tm-${hood.fingerprint.slice(0, 12)}`;
+	const name = `Tubemail Peer ${hood.id.slice(0, 7)} (${hood.info.subject.commonName})`;
 
 	// Advertisment of the local peer
+	debug('publishing type="%s" port=%d name="%s"', type, port, name);
 	mdns.publish({type, port, name});
 
 	// Searching for remote peers
